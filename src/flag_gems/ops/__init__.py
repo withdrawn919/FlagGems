8180406e2e5660474eb1cb2721bf361fc70ab294
withdrawn919 <871214699@qq.com>
2026-05-16 11:28:42 +0800

    fix code style

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -84,8 +84,8 @@ from flag_gems.ops.conv1d import conv1d
 from flag_gems.ops.conv2d import conv2d
 from flag_gems.ops.conv3d import conv3d
 from flag_gems.ops.conv_depthwise2d import _conv_depthwise2d
-from flag_gems.ops.conv_transpose2d import conv_transpose2d
 from flag_gems.ops.conv_transpose1d import conv_transpose1d
+from flag_gems.ops.conv_transpose2d import conv_transpose2d
 from flag_gems.ops.copy import copy, copy_
 from flag_gems.ops.copysign import copysign, copysign_out
 from flag_gems.ops.cos import cos, cos_
